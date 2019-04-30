7525bc7e88a8537df98768b39aef5f6d768afa8c
yotammanne <yotammanne@mail.tau.ac.il>
2019-04-30 18:14:05 +0300

    first commit

---
+++ hmm.py
@@ -0,0 +1,66 @@
+from data import *
+import time
+
+
+def hmm_train(sents):
+    """
+        sents: list of tagged sentences
+        Returns: the q-counts and e-counts of the sentences' tags, total number of tokens in the sentences
+    """
+
+    print "Start training"
+    total_tokens = 0
+    q_tri_counts, q_bi_counts, q_uni_counts, e_word_tag_counts,e_tag_counts = {}, {}, {}, {}, {}
+    ### YOUR CODE HERE
+    raise NotImplementedError
+    ### END YOUR CODE
+    return total_tokens, q_tri_counts, q_bi_counts, q_uni_counts, e_word_tag_counts,e_tag_counts
+
+def hmm_viterbi(sent, total_tokens, q_tri_counts, q_bi_counts, q_uni_counts, e_word_tag_counts,e_tag_counts, lambda1, lambda2):
+    """
+        Receives: a sentence to tag and the parameters learned by hmm
+        Returns: predicted tags for the sentence
+    """
+    predicted_tags = [""] * (len(sent))
+    ### YOUR CODE HERE
+    raise NotImplementedError
+    ### END YOUR CODE
+    return predicted_tags
+
+def hmm_eval(test_data, total_tokens, q_tri_counts, q_bi_counts, q_uni_counts, e_word_tag_counts,e_tag_counts):
+    """
+    Receives: test data set and the parameters learned by hmm
+    Returns an evaluation of the accuracy of hmm
+    """
+    print "Start evaluation"
+    acc_viterbi = 0.0
+    ### YOUR CODE HERE
+    raise NotImplementedError
+    ### END YOUR CODE
+
+    return acc_viterbi
+
+if __name__ == "__main__":
+    start_time = time.time()
+    train_sents = read_conll_pos_file("Penn_Treebank/train.gold.conll")
+    dev_sents = read_conll_pos_file("Penn_Treebank/dev.gold.conll")
+    vocab = compute_vocab_count(train_sents)
+
+    train_sents = preprocess_sent(vocab, train_sents)
+    dev_sents = preprocess_sent(vocab, dev_sents)
+
+    total_tokens, q_tri_counts, q_bi_counts, q_uni_counts, e_word_tag_counts, e_tag_counts = hmm_train(train_sents)
+    acc_viterbi = hmm_eval(dev_sents, total_tokens, q_tri_counts, q_bi_counts, q_uni_counts, e_word_tag_counts,e_tag_counts)
+    print "HMM DEV accuracy: " + str(acc_viterbi)
+
+    train_dev_end_time = time.time()
+    print "Train and dev evaluation elapsed: " + str(train_dev_end_time - start_time) + " seconds"
+
+    if os.path.exists("Penn_Treebank/test.gold.conll"):
+        test_sents = read_conll_pos_file("Penn_Treebank/test.gold.conll")
+        test_sents = preprocess_sent(vocab, test_sents)
+        acc_viterbi = hmm_eval(test_sents, total_tokens, q_tri_counts, q_bi_counts, q_uni_counts,
+                                           e_word_tag_counts, e_tag_counts)
+        print "HMM TEST accuracy: " + str(acc_viterbi)
+        full_flow_end_time = time.time()
+        print "Full flow elapsed: " + str(full_flow_end_time - start_time) + " seconds"
