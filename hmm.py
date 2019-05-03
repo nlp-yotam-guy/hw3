2375a189ae2f194b36e82ce27e5acc6eba942b70
guyazov <guyazov@gmail.com>
2019-05-03 09:42:35 +0300

    hmm_train

--- hmm.py
+++ hmm.py
@@ -1,6 +1,13 @@
 from data import *
 import time
 
+def add_to_dict(key,dict):
+    if key in dict:
+        dict[key] += 1
+        return 0
+    else:
+        dict[key] = 1
+        return 1
 
 def hmm_train(sents):
     """
@@ -12,7 +19,22 @@ def hmm_train(sents):
     total_tokens = 0
     q_tri_counts, q_bi_counts, q_uni_counts, e_word_tag_counts,e_tag_counts = {}, {}, {}, {}, {}
     ### YOUR CODE HERE
-    raise NotImplementedError
+    for sentence in sents:
+        for j in range(2, len(sentence)):
+            add_to_dict((sentence[j - 2][1], sentence[j - 1][1], sentence[j][1]), q_tri_counts)
+            add_to_dict((sentence[j - 1][1], sentence[j][1]), q_bi_counts)
+            add_to_dict((sentence[j][1]), q_uni_counts)
+            add_to_dict((sentence[j][0], sentence[j][1]), e_word_tag_counts)
+            add_to_dict(sentence[j][1], e_tag_counts)
+            total_tokens += 1
+        add_to_dict((sentence[0][1], sentence[1][1]), q_bi_counts)
+        add_to_dict((sentence[0][1]), q_uni_counts)
+        add_to_dict((sentence[1][1]), q_uni_counts)
+        add_to_dict((sentence[0][0], sentence[0][1]), e_word_tag_counts)
+        add_to_dict((sentence[1][0], sentence[1][1]), e_word_tag_counts)
+        add_to_dict(sentence[0][1], e_tag_counts)
+        add_to_dict(sentence[1][1], e_tag_counts)
+        total_tokens += 2
     ### END YOUR CODE
     return total_tokens, q_tri_counts, q_bi_counts, q_uni_counts, e_word_tag_counts,e_tag_counts
 
